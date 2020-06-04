d78865ff400df384cc3a6f008ff6a620aeaf189a
John Pham <mr.john.pham@gmail.com>
2020-06-04 06:05:17 -0400

    Successfully implement dynamic links

--- landing_page.js
+++ landing_page.js
@@ -2,11 +2,13 @@
 
 (function () {
     let programsContainer = document.querySelector("#programs-container"); //look for programs-container ID
+    let navBar = document.querySelector("#nav-bar");
     let sections = programsContainer.children; //access the HTML collection that contains the sections
-    //console.log(sections);
     for (let i = 0; i < sections.length; i++) {
         let currentString = sections[i].children[0].innerText; //access each section h3 heading text
         let currentStringUnderscores = currentString.replace(" ", "_"); //replace spaces with underscores
-        console.log(`<a href="#${currentStringUnderscores}">${currentString}</a>`); //output HTML links
+        let linkTag = document.createElement("a"); //create link tag
+        linkTag.innerHTML = '<a href="#' + currentStringUnderscores + '">' + currentString + '</a>'; //create HTML for link
+        navBar.appendChild(linkTag); //append link tag to the navigation bar
     }
 })();
