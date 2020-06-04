f6ee19f950394056c0eb909631e2be58d3a8e109
John Pham <mr.john.pham@gmail.com>
2020-06-04 05:52:49 -0400

    Access section headings via JavaScript

--- landing_page.js
+++ landing_page.js
@@ -1,2 +1,12 @@
 "use strict";
 
+(function () {
+    console.log("Hello world")
+    let programsContainer = document.querySelector("#programs-container");
+    //console.log(programsContainer.children[0].children[0].innerText);
+    let sectionHeadings = programsContainer.children;
+    //console.log(sectionHeadings);
+    for (let i = 0; i < sectionHeadings.length; i++) {
+        console.log(sectionHeadings[i].children[0].innerText);
+    }
+})();
