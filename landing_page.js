07dca0302cbca40738eb8e1e20526f56db93fa13
John Pham <mr.john.pham@gmail.com>
2020-06-04 05:59:13 -0400

    Start writing algorithm to output dynamic HTML links

--- landing_page.js
+++ landing_page.js
@@ -1,13 +1,12 @@
 "use strict";
 
 (function () {
-    console.log("Hello world")
-    let programsContainer = document.querySelector("#programs-container");
-    //console.log(programsContainer.children[0].children[0].innerText);
-    let sectionHeadings = programsContainer.children;
-    //console.log(sectionHeadings);
-    for (let i = 0; i < sectionHeadings.length; i++) {
-        let currentString = sectionHeadings[i].children[0].innerText;
-        console.log(currentString);
+    let programsContainer = document.querySelector("#programs-container"); //look for programs-container ID
+    let sections = programsContainer.children; //access the HTML collection that contains the sections
+    //console.log(sections);
+    for (let i = 0; i < sections.length; i++) {
+        let currentString = sections[i].children[0].innerText; //access each section h3 heading text
+        let currentStringUnderscores = currentString.replace(" ", "_"); //replace spaces with underscores
+        console.log(`<a href="#${currentStringUnderscores}">${currentString}</a>`); //output HTML links
     }
 })();
